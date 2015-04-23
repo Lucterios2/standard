068274ab67a1d6a364161857a31b45b2e4919ab3
Laurent GAY <l.gay@sd-libre.fr>
2015-04-23 18:05:31 +0200

    search with custom field

--- lucterios/standard/appli_settings.py
+++ lucterios/standard/appli_settings.py
@@ -11,7 +11,13 @@ from os.path import join, dirname
 import lucterios.standard
 
 def get_subtitle():
-    return ugettext("Generic management application")
+    try:
+        from django.apps.registry import apps
+        legalentity = apps.get_model("contacts", "LegalEntity")
+        our_detail = legalentity.objects.get(id=1)
+        return our_detail.name
+    except LookupError:
+        return ugettext("Generic management application")
 
 APPLIS_NAME = lucterios.standard.__title__()
 APPLIS_VERSION = lucterios.standard.__version__
