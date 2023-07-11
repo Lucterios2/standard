b3a20cae5f5ca4c3f1eef628d89c139c32c92600
Laurent GAY <l.gay@sd-libre.fr>
2023-07-11 12:34:07 +0200

    support Python 3.11 / end for 3.6

--- setup.py
+++ setup.py
@@ -49,11 +49,11 @@ setup(
         'Natural Language :: English',
         'Natural Language :: French',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Database :: Front-Ends',
     ],
     # Packages
